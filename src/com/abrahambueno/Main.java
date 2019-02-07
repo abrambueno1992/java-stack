722d23707b227fdd74f701f6ba91f370c322962f
Abraham Bueno <abueno@localhost.localdomain>
2019-02-07 16:45:55 -0600

    Work in progress

---
+++ src/com/abrahambueno/Main.java
@@ -0,0 +1,16 @@
+package com.abrahambueno;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Stack testing = new Stack();
+        System.out.println("Number of items: " + testing.x);
+        testing.push("first");
+        testing.push("second");
+        testing.push("third");
+        System.out.println("Number of items: " + testing.x);
+        System.out.println("Print the stack: " + Arrays.toString(testing.getStack()));
+    }
+}
---
+++ src/com/abrahambueno/Stack.java
@@ -0,0 +1,79 @@
+package com.abrahambueno;
+
+import java.util.Arrays;
+
+public class Stack {
+    private int maxStack = 1000;
+    private String[] stack;
+    int x = 0;
+
+
+    public void push(String word) {
+        if (x >= 1000) {
+            System.out.println("MAXIMUM STACK SIZE OF 1000 REACHED");
+        } else {
+            stack = biggerArray(word);
+
+        }
+
+    }
+    public String[] biggerArray(String arr) {
+        // increment counter
+        x++;
+        // new bigger array
+        String[] biggerStack = new String[x];
+        // populate the array with the words
+        // max index = x - 1
+        for (int i = 0; i < x - 1 ; i++) {
+            biggerStack[i] = stack[i];
+        }
+        // push the word into the arr
+        biggerStack[x - 1] = arr;
+        return biggerStack;
+    }
+
+    public void pop() {
+        stack = newArray(stack);
+    }
+    public String[] newArray(String[] arr) {
+        if (arr == null || x < 0 || x >= arr.length) {
+            return arr;
+        }
+        // decrement counter
+        x--;
+        // new array
+        String[] newStack = new String[x];
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            newStack[i] = arr[i];
+        }
+
+
+        // return new array
+        return newStack;
+    }
+
+    public String[] getStack() {
+        return stack;
+    }
+
+    public void setStack(String[] stack) {
+        this.stack = stack;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public String toString() {
+        return "Stack{" +
+                "stack=" + Arrays.toString(stack) +
+                ", x=" + x +
+                '}';
+    }
+}
